b48a9f2b0dcdc26aa42c8011018d9e808da2622a
Aditya Kumar Vishwakarma <vishwakarmaaditya03@gmail.com>
2025-08-05 13:06:04 +0530

    feat: add user model in the application

--- backend/apps/users/models.py
+++ backend/apps/users/models.py
@@ -1,3 +1,9 @@
 from django.db import models
+from django.contrib.auth.models import User 
+class UserProfile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    auth0_user_id = models.CharField(max_length=100, unique=True)  # maps to Auth0 user_id (sub)
+    picture = models.URLField(blank=True, null=True)
 
-# Create your models here.
+    def __str__(self):
+        return self.user.username
