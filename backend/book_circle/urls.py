c63ba223fb9419a564f76e3225343cf3e41da53b
Aditya Kumar Vishwakarma <vishwakarmaaditya03@gmail.com>
2025-08-05 13:05:41 +0530

    feat: add auth0 + JWT in the api

--- backend/book_circle/urls.py
+++ backend/book_circle/urls.py
@@ -16,8 +16,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from apps.users import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api-auth/', include('rest_framework.urls'))
+    path('api/public', views.public),
+    path('api/private', views.private),
+    path('api/private-scoped', views.private_scoped)
 ]
