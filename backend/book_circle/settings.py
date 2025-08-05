61bdb8bdcdee5f232ea3d738819976bb9b74a62b
Aditya Kumar Vishwakarma <vishwakarmaaditya03@gmail.com>
2025-08-05 17:56:28 +0530

    feat: add book model in the project

--- backend/book_circle/settings.py
+++ backend/book_circle/settings.py
@@ -43,7 +43,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
-    'apps.users'
+    'apps.users',
+    'apps.books'
 ]
 
 MIDDLEWARE = [
